ab47c1561a30096984ddb64de53ae391fceca741
yaxp9 <yaxp9951@gmail.com>
2026-08-26 18:07:40 -0600

    Add DBConnection class, verified JDBC connection to MySQL works

--- src/main/java/com/yax/library/util/DBConnection.java
+++ src/main/java/com/yax/library/util/DBConnection.java
@@ -1,4 +1,51 @@
 package com.yax.library.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class DBConnection {
+
+    public static Connection getConnection() throws SQLException {
+        String dbUrl, dbUser, dbPass;
+
+        try (InputStream in = DBConnection.class.getClassLoader()
+                .getResourceAsStream("db.properties")) {
+
+            if (in == null) {
+                throw new RuntimeException("db.properties not found on classpath");
+            }
+
+            Properties prop = new Properties();
+            prop.load(in);
+            dbUrl = prop.getProperty("db.url");
+            dbUser = prop.getProperty("db.user");
+            dbPass = prop.getProperty("db.password");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return DriverManager.getConnection(dbUrl, dbUser, dbPass);
+    }
+
+    public static void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        try (Connection conn = getConnection()) {
+            System.out.println("Connected successfully!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
